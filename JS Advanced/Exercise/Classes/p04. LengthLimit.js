57e7730e07fe288813db2586df13764ee701c7f8
Hristo Panev <hristopanef@gmail.com>
2019-06-25 15:45:04 +0200

    Classes

---
+++ JS Advanced/Exercise/Classes/p04. LengthLimit.js
@@ -0,0 +1,45 @@
+class Stringer {
+    constructor(innerString, innerLength){
+        this.innerString = innerString;
+        this.innerLength = innerLength;
+    }
+
+    increase(length){
+        this.innerLength += length;
+    }
+
+    decrease(length){
+        this.innerLength -= length;
+
+        if (this.innerLength < 0) {
+            this.innerLength = 0;
+        }
+    }
+
+    toString(){
+        let output = this.innerString;
+
+        if (this.innerLength === 0) {
+            output = '...';
+        }else if (this.innerString.length > this.innerLength) {
+            let cutStr = this.innerString.length - this.innerLength;
+
+            output = this.innerString.substring(0, cutStr);
+            output += '...';
+        }
+
+        return output;
+    }
+}
+
+let test = new Stringer("Test", 5);
+console.log(test.toString()); // Test
+
+test.decrease(3);
+console.log(test.toString()); // Te...
+
+test.decrease(5);
+console.log(test.toString()); // ...
+
+test.increase(4);
+console.log(test.toString()); // Test

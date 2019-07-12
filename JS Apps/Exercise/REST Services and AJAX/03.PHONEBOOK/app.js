bcaccc40e0c8b25b3af032c978b659be83791631
Hristo Panev <hristopanef@gmail.com>
2019-07-12 15:07:21 +0200

    REST Services and AJAX

--- JS Apps/Exercise/REST Services and AJAX/03.PHONEBOOK/app.js
+++ JS Apps/Exercise/REST Services and AJAX/03.PHONEBOOK/app.js
@@ -1,33 +1,80 @@
 function attachEvents() {
-    document.getElementById('btnLoad')
-        .addEventListener('click', function () {
+    let loadButton = document.getElementById('btnLoad');
+    let phoneBook = document.getElementById('phonebook');
 
+    loadButton.addEventListener('click', function () {
+        reloadFunction();
+    });
+
+    function reloadFunction() {
+        phoneBook.innerHTML = '';
+        let url = `https://phonebook-nakov.firebaseio.com/phonebook.json`;
+
+        fetch(url)
+            .then((request) => request.json())
+            .then((data) => {
+                let keys = Object.keys(data);
+
+
+                for (let key of keys) {
+                    let person = data[key];
+                    let name = person.person;
+                    let phoneNumber = person.phone;
+
+                    let phoneBook = document.getElementById('phonebook');
+                    let buttonDel = document.createElement('button');
+                    buttonDel.textContent = 'DELETE';
+
+                    let li = document.createElement('li');
+
+                    li.textContent = `${name}: ${phoneNumber}`;
+                    phoneBook.appendChild(li);
+
+                    buttonDel.addEventListener('click', function () {
+                        deleteData(key);
+                    });
+
+                    li.appendChild(buttonDel);
+                }
+
+            })
+    }
+
+    document.getElementById('btnCreate')
+        .addEventListener('click', function () {
             let url = `https://phonebook-nakov.firebaseio.com/phonebook.json`;
 
-            fetch(url)
-                .then((request) => request.json())
-                .then((data) => {
-                    let values = Object.values(data);
+            let newContactJSON = JSON.stringify({
+                person: document.getElementById('person').value,
+                phone: document.getElementById('phone').value
+            });
 
-                    for (let value of values){
-                        let name = value.person;
-                        let phoneNumber = value.phone;
+            fetch(url, {
+                method: 'post',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: newContactJSON,
+            })
+                .then(x => x.json())
+                .then(x => {
+                    reloadFunction()
+                });
 
-                        let phoneBook = document.getElementById('phonebook');
+            document.getElementById('person').value = '';
+            document.getElementById('phone').value = '';
+        });
 
-                        let buttonDel = document.createElement('button');
-                        buttonDel.textContent = 'DELETE';
 
-                        let li = document.createElement('li');
-
-                        li.textContent = `${name}: ${phoneNumber}`;
-                        li.appendChild(buttonDel);
-
-                        phoneBook.appendChild(li);
-
-                    }
-                })
+    function deleteData(key) {
+        fetch(`https://phonebook-nakov.firebaseio.com/phonebook/${key}.json`, {
+            method: 'delete'
         })
+            .then(response => response.json())
+            .then(x => {
+                reloadFunction()
+            })
+    }
 }
 
 attachEvents();
--- JS Apps/Exercise/REST Services and AJAX/04.MESSENGER/app.js
+++ JS Apps/Exercise/REST Services and AJAX/04.MESSENGER/app.js
@@ -1,5 +1,67 @@
 function attachEvents() {
-    console.log('TODO...');
+    let url = 'https://rest-messanger.firebaseio.com/messanger.json';
+    let submit = document.getElementById('submit');
+    let messages = document.getElementById('messages');
+    let author = document.getElementById('author');
+    let contentInput = document.getElementById('content');
+    let refresh = document.getElementById('refresh');
+
+    function loadMessage() {
+            messages.innerHTML = '';
+            fetch(url)
+                .then((request) => request.json())
+                .then((data) => {
+                    // for (let post of Object.values(data)) {
+                    //     let name = post.author;
+                    //     let content = post.content;
+                    //
+                    //     messages.textContent = `${name}: ${content}\n`
+                    // }
+
+                    let post = Object.values(data);
+
+                    messages.textContent = post
+                        .map(value => `${value.author}: ${value.content}`)
+                        .join('\n');
+                })
+    }
+
+
+    function postMessage() {
+        let name = author.value;
+        let content = contentInput.value;
+        author.value = '';
+        contentInput.value = '';
+
+        let data = {
+            author: name,
+            content
+        };
+
+        if (name.length && content.length){
+
+            fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(data),
+            })
+                .then(x => x.json())
+                .then(x => {
+                    loadMessage()
+                })
+        }
+    }
+
+    refresh.addEventListener('click', function () {
+        loadMessage();
+    });
+
+    submit.addEventListener('click', function () {
+        postMessage()
+    });
+
 }
 
 attachEvents();

cae7e50961823e75349c3a28bc5cb46362469fdb
hristopanev <hristopanef@gmail.com>
2020-07-02 12:29:21 +0200

    refresh

--- JS Advanced/JS Advanced May 2020/Syntax, Functions And Statements/index.js
+++ JS Advanced/JS Advanced May 2020/Syntax, Functions And Statements/index.js
@@ -4,3 +4,29 @@ function solve () {
    const x = 23;
 
 };
+
+var obj = {
+   prop: 1,
+   prop2: 'string',
+   prop3: function() {
+
+   }
+}
+
+obj.prop;
+
+function createPerson(firstName, lastName) {
+   firstName = firstName || 'John';
+   lastName = lastName || 'Doe';
+
+   var person = {
+      fullName: firstName + ' ' + lastName
+   };
+   
+   return person;
+}
+
+// createPerson('', '')
+
+let num = 1;
+console.log(typeof num)

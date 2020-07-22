c223d538386d837f62125a70c5295ad7e2d54d7f
hristopanev <hristopanef@gmail.com>
2020-07-22 17:29:35 +0200

    exercise

--- JS Advanced/JS Advanced May 2020/Syntax, Functions And Statements/exercise.js
+++ JS Advanced/JS Advanced May 2020/Syntax, Functions And Statements/exercise.js
@@ -1,8 +1,8 @@
-function fruit (fruit, weight, money) {
+function fruit (fruit, weight, price) {
 
     let kg = weight * 0.001;
-    let neededMoney = kg * money;
-    let outputString = `I need $${neededMoney.toFixed(2)} to buy ${kg.toFixed(2)} kilograms ${fruit}.`;
+    let total = kg * price;
+    let outputString = `I need $${total.toFixed(2)} to buy ${kg.toFixed(2)} kilograms ${fruit}.`;
 
     console.log(outputString);
 }

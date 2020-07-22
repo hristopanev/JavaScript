725c8ad4511b8fe6d6e6ea511b868cde6e0796fc
hristopanev <hristopanef@gmail.com>
2020-07-22 19:48:55 +0200

    exercise

--- JS Advanced/JS Advanced May 2020/Syntax, Functions And Statements/exercise.js
+++ JS Advanced/JS Advanced May 2020/Syntax, Functions And Statements/exercise.js
@@ -9,3 +9,17 @@ function fruit (fruit, weight, price) {
 
 fruit("orange", 2500, 1.80);
 
+function gcd(a, b) {
+    const smallNumber = Math.min(a, b);
+    let gcdNumber = 1;
+
+    for (let i = 1; i <= smallNumber; i++) {
+        if(a % i == 0 && b % i == 0) {
+            gcdNumber = i;
+        }
+    }
+
+    console.log(gcdNumber);
+}
+
+gcd(2154, 458)

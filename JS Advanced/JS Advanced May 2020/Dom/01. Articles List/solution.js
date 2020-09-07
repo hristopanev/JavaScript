27fc45c0523db5ab51533871caad8ca61a052872
hristopanev <hristopanef@gmail.com>
2020-09-07 08:36:10 +0200

    dom

--- JS Advanced/JS Advanced May 2020/Dom/01. Articles List/solution.js
+++ JS Advanced/JS Advanced May 2020/Dom/01. Articles List/solution.js
@@ -1,4 +1,31 @@
 function createArticle() {
+	const createTittleInput = document.getElementById('createTitle');
+	const createContentInput = document.getElementById('createContent');
+	const articleSection = document.getElementById('articles');
+
+	const titleInputValue = createTittleInput.value;
+	const contentValue = createContentInput.value;
+
+	if(!titleInputValue || !contentValue) {
+		alert('Please enter text!');
+		return;
+	}
 	
-	//TODO...
+
+	const newArticle = document.createElement('article');
+	const articleHeading = document.createElement('h3');
+	const articleParagraph = document.createElement('p');
+
+	newArticle.appendChild(articleHeading);
+	newArticle.appendChild(articleParagraph);
+
+	articleSection.appendChild(newArticle);
+	
+	articleHeading.innerText= titleInputValue;
+	articleParagraph.innerText = contentValue;
+
+	createTittleInput.value = '';
+	createContentInput.value = '';
+
+
 }
--- JS Advanced/JS Advanced May 2020/Dom/04. Visited Websites/solution.js
+++ JS Advanced/JS Advanced May 2020/Dom/04. Visited Websites/solution.js
@@ -1,4 +1,21 @@
-function solve() {
 
-  //TODO...
+function solve() {
+  const anchors = Array.from(document.querySelectorAll('a'));
+  const counters = anchors.map(el => +el.nextElementSibling.innerHTML.split(' ')[1]);
+  // const counters = new Array(anchors.length).fill(0);
+  const container = document.querySelector('#page1 > .middled');
+  container.addEventListener('click', function (e) {
+    const target = e.target;
+    const parent = e.target.parentElement;
+    const notTargetAnchor = e.target.localName !== 'a';
+    const notTargetParentAnchor = parent && parent.localName !== 'a';
+    if (notTargetAnchor && notTargetParentAnchor) { return }
+    const targetAnchor = notTargetAnchor ? parent : target;
+    const counterIndex = anchors.indexOf(targetAnchor);
+    if (counterIndex === -1) { return; }
+    counters[counterIndex]++;
+    const pargraph = targetAnchor.nextElementSibling;
+    pargraph.innerText = `visited ${counters[counterIndex]} times`;
+  });
+
 }
